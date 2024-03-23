b0b820aeb06836abbec2fc12b1d6e7dc3cd82b01
Ritvik <ritvik.gupta2205@gmail.com>
2024-03-23 18:06:25 +0000

    fix commit issue 2

---
+++ settings.py
@@ -0,0 +1,28 @@
+import pygame as pg
+
+vec = pg.math.Vector2
+
+FPS = 60
+FIELD_COLOR = (48, 39, 32)
+
+SPRITE_DIR_PATH = 'assets/sprites'
+
+ANIM_TIME_INTERVAL = 200 #millisecs
+FAST_ANIM_TIME_INTERVAL = 50 #millisecs
+
+TILE_SIZE = 50
+FIELD_SIZE = FIELD_W, FIELD_H = 10, 20
+FIELD_RES = FIELD_W * TILE_SIZE, FIELD_H * TILE_SIZE
+
+INIT_POS_OFFSET = vec(FIELD_W // 2 - 1, 0)
+MOVE_DIRECTIONS = {'left' : vec(-1,0), 'right' : vec(1,0), 'down' : vec(0,1)}
+
+TETROMINOES = {
+    'L' : [(0,0), (-1,0), (-1,1), (-1,2)],
+    'J' : [(0,0), (-1,0), (0,1), (0,2)],
+    'T' : [(0,0), (-1,0), (1,0), (0,1)],
+    'O' : [(0,0), (0,1), (-1,0), (-1,1)],
+    'S' : [(0,0), (-1,0), (0,1), (1,1)],
+    'Z' : [(0,0), (1,0), (0,1), (-1,1)],
+    'I' : [(0,0), (0,1), (0,-1), (0,-2)]
+}
